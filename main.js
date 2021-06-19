139b5364ea144dcca281a0157cdbff10d09e5745
Sebastian Salazar <salazars804@gmail.com>
2021-06-19 18:49:26 -0500

    bugs test

--- main.js
+++ main.js
@@ -30,7 +30,6 @@ app.listen(puerto || 5000, () => {
 });
 
 //--- Cierra parte del frontend ---
-
 server.listen(puertoServer || 3000, () => {
   console.log("Servidor server esta escuchando");
 });
